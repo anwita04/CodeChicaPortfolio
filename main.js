31bd52e33d113981e043b8b2084ed59eacb0d09f
anwita04 <53347208+anwita04@users.noreply.github.com>
2019-07-26 11:18:05 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+document.getElementById("btn1").addEventListener("click", myName)
+document.getElementById("btn2").addEventListener("click", mySchool)
+document.getElementById("btn3").addEventListener("click", myCareer)
+document.getElementById("btn4").addEventListener("click", myHobbies)
+
+
+function myName() {
+    document.getElementById("name").innerHTML = "Who am I? No one knows how to answer that and neither do I, because everyday I learn more and more about myself. I am a girl who likes to call herself a feminist, a global citizen, a socialist. And I find more things to pe a part of everyday. I am a global volunteer for girlforward and STAR. A feminist by choice and a socialist by voice.";
+}
+
+function mySchool() {
+    document.getElementById("school").innerHTML = "Some of my passions include learning cultures of the world and languages of religion. I also enjoy expressing myself through a classical dance and my strenght through TaeKwonDo. I am also really passionate about a club where we model the UN, in which we develop our skills to  promate peace in our daily lives.";
+}
+
+function myCareer() {
+    document.getElementById("career").innerHTML = "In the future I plan to study international relation, and foreign affairs and take part in some diplomatic career or future. I also plan to take my coding learning futher and implemet that into the change I want to bring.";
+}
+
